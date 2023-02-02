b66a0c90e8d7abd0ece5cbf6211999d9fcf1ac44
ChristineAnne-Adhiambo <annechristineadhiambo@gmail.com>
2023-02-02 09:53:36 +0300

    basic data types

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    var studentFullName = "Christine"
+    var age =  22
+    var phoneNumber = "0725436789"
+    var weightInKg = 63
+    val isKenyan = true
+    val isNotKenyan = false
+
+    println(studentFullName)
+    println(age)
+    println(phoneNumber)
+    println(weightInKg)
+    println(isKenyan)
+    println(isNotKenyan)
+
+
+}
